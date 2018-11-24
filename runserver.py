e0a1c80468628b8abfcff348403a4a60767d2fb1
mastercharizardx <44578852+mastercharizardx@users.noreply.github.com>
2018-11-24 20:00:46 +0530

    Add files via upload

--- runserver.py
+++ runserver.py
@@ -5,10 +5,11 @@ This script runs the MoodMoviesWebApp application using a development server.
 from os import environ
 from MoodMoviesWebApp import app
 
-if __name__ == '__main__':
+def runappserver():
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
+	
